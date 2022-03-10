a9bdce7e984a1c5265ee050d70c6dea7bac46e3e
samjgorman <samjgorman@gmail.com>
2022-03-09 18:52:13 -0800

    Refactor to class

--- web_scraper.py
+++ web_scraper.py
@@ -1,6 +1,9 @@
 '''
 This is a web scraper for RSS feed(s) to demonstrate multithreading.
 
+Sources:
+https://www.jcchouinard.com/read-rss-feed-with-python/
+
 '''
 
 from wsgiref.headers import Headers
@@ -9,12 +12,6 @@ import lxml
 from bs4 import BeautifulSoup
 from threading import Thread, Lock, Semaphore
 import time
-import string
-from random import choice
-
-
-# RSS_FEED_URLS = [
-#     'https://www.nydailynews.com/arcio/rss/category/news/?sort=display_date:desc', 'https://www.nydailynews.com/arcio/rss/category/opinion/?sort=display_date:desc', 'https://www.nydailynews.com/arcio/rss/category/sports/?sort=display_date:desc']
 
 RSS_FEED_URLS = [
     'https://www.france24.com/en/asia-pacific/rss', 'https://www.france24.com/en/africa/rss', 'https://www.france24.com/en/france/rss', 'https://www.france24.com/en/sport/rss']
@@ -26,63 +23,67 @@ header = {
 }
 
 
-def addToIndex(data, indexLock, index):
-    # Protect critical regions of code...
-    with indexLock:
-        index.append(data)
+class ThreadedRSSFeed:
 
+    def __cleanup(self):
+        for thread in self.threads:
+            thread.join()
 
-def downloadUrl(articleUrl, index, indexLock):
-    try:
-        request = requests.get(articleUrl, headers=header)
-    except Exception as e:
-        print(e)
-    else:
-        # Simulate parsing over a network
-        time.sleep(0.10)
-        # TODO: parse data from each article page
-        soup = BeautifulSoup(request.text)
-        paragraphs = soup.find('article').findAll('p')
-        for paragraph in paragraphs:
-            # print(paragraph)
-            addToIndex(paragraph, indexLock, index)
+    def __addToIndex(self, data):
+        # Protect critical regions of code...
+        with self.indexLock:
+            self.index.append(data)
 
+    def __downloadUrl(self, articleUrl):
+        try:
+            request = requests.get(articleUrl, headers=self.headers)
+        except Exception as e:
+            print(e)
+        else:
+            # Simulate parsing over a network
+            time.sleep(0.10)
+            # TODO: parse data from each article page
+            soup = BeautifulSoup(request.text)
+            paragraphs = soup.find('article').findAll('p')
+            for paragraph in paragraphs:
+                self.__addToIndex(paragraph)
 
-def cleanup(threads):
-    for thread in threads:
-        thread.join()
+    def __dispatchThreads(self):
+        for url in self.articleUrls:
+            # TODO: Threadpool
+            thread = Thread(target=self.__downloadUrl, args=(url,))
+            self.threads.append(thread)
+            thread.start()
+        self.__cleanup()
 
+    def __init__(self, RSSArray, headers):
+        self.RSSArray = RSSArray
+        self.headers = headers
+        self.articleUrls = []
+        self.index = []
+        self.indexLock = Lock()
+        self.threads = []
 
-def main():
-    allUrls = []
-    for feedUrl in RSS_FEED_URLS:
-        request = requests.get(feedUrl, headers=header)
-        print(request.status_code)
-        if request.status_code == 200:
-            try:
-                soup = BeautifulSoup(request.text, 'lxml')
-            except Exception as e:
-                print('Could not parse the xml: ', request.url)
-                print(e)
-            # Source: https://www.jcchouinard.com/read-rss-feed-with-python/
+        for RSSfeed in self.RSSArray:
+            request = requests.get(RSSfeed, headers=self.headers)
+            if request.status_code == 200:
+                try:
+                    soup = BeautifulSoup(request.text, 'lxml')
+                except Exception as e:
+                    print('Could not parse the xml: ', request.url)
+                    print(e)
             articles = soup.findAll('item')
             articles_dicts = [{'title': a.find('title').text, 'link': a.link.nextSibling.replace('\n', '').replace('\t', '').strip(), 'description': a.find(
                 'description').text, 'pubdate': a.find('pubdate').text} for a in articles]
             urls = [d['link'] for d in articles_dicts if 'link' in d]
             titles = [d['title'] for d in articles_dicts if 'title' in d]
-            allUrls += (urls)
+            self.articleUrls += (urls)
+        self.__dispatchThreads()
 
-    # print(allUrls)
-    threads = []
-    index = []
-    indexLock = Lock()
-    for url in allUrls:
-        # TODO: Threadpool
-        thread = Thread(target=downloadUrl, args=(url, index, indexLock))
-        threads.append(thread)
-        thread.start()
-    cleanup(threads)
-    print(index)
+
+def main():
+    feed = ThreadedRSSFeed(RSS_FEED_URLS, header)
+    print(feed.index)
 
 
 if __name__ == "__main__":
